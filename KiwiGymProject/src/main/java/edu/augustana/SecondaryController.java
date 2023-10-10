c2f664d06b356e02ae2954ac4c41186b80de99e5
Ethan <ethansosnowski22@augustana.edu>
2023-10-09 22:40:36 -0500

    finished the home screen adn merged with the create a plan screen

--- KiwiGymProject/src/main/java/edu/augustana/SecondaryController.java
+++ KiwiGymProject/src/main/java/edu/augustana/SecondaryController.java
@@ -2,11 +2,13 @@ package edu.augustana;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class SecondaryController {
-
     @FXML
-    private void switchToPrimary() throws IOException {
+    private Button backButton;
+    @FXML
+    private void backButtonAction() throws IOException {
         App.setRoot("primary");
     }
 }
